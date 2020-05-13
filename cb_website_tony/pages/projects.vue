bffb4253b253d3eff96719ed38ddd49cfc44b165
Tony-S201 <swierz.tony@gmail.com>
2020-05-13 15:30:48 +0200

    structure projects mobile

--- cb_website_tony/pages/projects.vue
+++ cb_website_tony/pages/projects.vue
@@ -33,23 +33,21 @@
     <div class="project-card">
       <div class="project-card-image-container">
         <figure class="project-card-image">
-          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+          <img src="~/assets/img/fat-1.jpg" alt="Placeholder image">
         </figure>
       </div>
       <div class="project-card-content-container">
         <div class="project-card-informations">
-          <p class="project-card-title">FAT</p>
-          <p class="project-card-function">Dev Front</p>
+          <p class="project-card-title">The FAT Bastard Gangbang</p>
+          <p class="project-card-function">Développeur Front-End</p>
         </div>
         <div class="project-card-content">
           <b-taglist>
-              <b-tag type="is-info">First</b-tag>
-              <b-tag type="is-info">Second</b-tag>
-              <b-tag type="is-info">Third</b-tag>
-              <b-tag type="is-info">Fourth</b-tag>
-              <b-tag type="is-info">Fifth</b-tag>
+              <b-tag type="is-info">React</b-tag>
+              <b-tag type="is-info">WordPress</b-tag>
+              <b-tag type="is-info">SASS</b-tag>
           </b-taglist>
-          <time datetime="2016-1-1">Jan 2016</time>
+          <time class="project-card-date" datetime="2016-1-1">Avril 2019</time>
         </div>
       </div>
     </div>
