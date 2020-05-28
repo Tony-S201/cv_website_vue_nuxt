2bc0ea89945c921f331956953823f1c0b42e5c7d
Tony-S201 <swierz.tony@gmail.com>
2020-05-28 17:51:02 +0200

    finish contact section

--- cb_website_tony/pages/about.vue
+++ cb_website_tony/pages/about.vue
@@ -2,7 +2,7 @@
   <section class="about-container">
     <h1 class="section-titles">About</h1>
     <div>
-      <h2 class="subtitle about-titles"
+      <h2 class="subtitle about-titles has-text-weight-bold"
         data-sal="fade"
         data-sal-delay="200"
         data-sal-duration="800"><img class="icon is-large about-title-pictures" src="~/assets/img/about-icons/icons8-administrateur-homme-100.png"/>{{ langs[languageActive].about.aboutperson.title }}</h2>
@@ -13,7 +13,7 @@
         data-sal-duration="1000">{{ langs[languageActive].about.aboutperson.text }}</p>
       <br>
       
-      <h2 class="subtitle about-titles"
+      <h2 class="subtitle about-titles has-text-weight-bold"
         data-sal="fade"
         data-sal-delay="200"
         data-sal-duration="800"><img class="icon is-large about-title-pictures" src="~/assets/img/about-icons/icons8-ordinateur-100.png"/>{{ langs[languageActive].about.work.title }}</h2>
@@ -23,7 +23,7 @@
         data-sal-duration="1000">{{ langs[languageActive].about.work.text }}</p>
       <br>
       
-      <h2 class="subtitle about-titles"
+      <h2 class="subtitle about-titles has-text-weight-bold"
         data-sal="fade"
         data-sal-delay="200"
         data-sal-duration="800"><img class="icon is-large about-title-pictures" src="~/assets/img/about-icons/icons8-étagère-à-livres-100.png"/>{{ langs[languageActive].about.study.title }}</h2>
@@ -33,7 +33,7 @@
         data-sal-duration="1000">{{ langs[languageActive].about.study.text }}</p>
       <br>
 
-      <h2 class="subtitle about-titles"
+      <h2 class="subtitle about-titles has-text-weight-bold"
         data-sal="fade"
         data-sal-delay="200"
         data-sal-duration="800"><img class="icon is-large about-title-pictures" src="~/assets/img/about-icons/icons8-masque-tuba-100.png"/>{{ langs[languageActive].about.hobbies.title }}</h2>
--- cb_website_tony/pages/contact.vue
+++ cb_website_tony/pages/contact.vue
@@ -4,20 +4,51 @@
     <div class="contact-flex-container">
       
       <div class="contact-form-container">
-        <b-field horizontal label="Subject" type="is-danger" message="Please enter a subject">
-          <b-input name="subject" expanded></b-input>
+        <h2 class="subtitle contact-titles has-text-weight-bold"
+          data-sal="fade"
+          data-sal-delay="200"
+          data-sal-duration="800">Formulaire</h2>
+        <b-field data-sal="slide-up"
+          data-sal-delay="800"
+          data-sal-duration="1000" label="Subject" label-position="on-border">
+          <b-input user-scalable="no" 
+                   name="subject" 
+                   placeholder="Subject"
+                   maxlength="50"
+                   expanded
+                   >
+          </b-input>
         </b-field>
 
-        <b-field horizontal label="From">
-          <b-input name="name" placeholder="Name" expanded></b-input>
-          <b-input name="email" type="email" placeholder="nobody@nowhere.com" expanded></b-input>
+        <b-field data-sal="slide-up"
+          data-sal-delay="800"
+          data-sal-duration="1000" label="Name" label-position="on-border">
+            <b-input value="" 
+              placeholder="Name"
+              maxlength="30"
+              >
+            </b-input>
         </b-field>
 
-        <b-field horizontal label="Message">
-          <b-input type="textarea"></b-input>
+        <b-field data-sal="slide-up"
+          data-sal-delay="800"
+          data-sal-duration="1000" label="Email" label-position="on-border">
+            <b-input type="email"
+                placeholder="john.doe@gmail.com"
+                maxlength="30"
+                required>
+            </b-input>
         </b-field>
 
-        <b-field horizontal>
+        <b-field data-sal="slide-up"
+          data-sal-delay="800"
+          data-sal-duration="1000" label="Message" label-position="on-border">
+          <b-input user-scalable="no" type="textarea" placeholder="Message..." required></b-input>
+        </b-field>
+
+        <b-field data-sal="slide-up"
+          data-sal-delay="800"
+          data-sal-duration="1000">
           <p class="control">
             <button class="button is-primary">
               Send message
@@ -27,7 +58,15 @@
       </div>
 
       <div class="contact-map-container">
-        
+        <h2 class="subtitle contact-titles has-text-weight-bold"
+          data-sal="fade"
+          data-sal-delay="200"
+          data-sal-duration="800">Localisation</h2>
+        <iframe data-sal="slide-up"
+          data-sal-delay="800"
+          data-sal-duration="1000" 
+          class="contact-map" 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22647.28890038643!2d-0.5660534772297189!3d44.803000105737894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5526f212045a41%3A0x40665174816ee80!2zQsOoZ2xlcw!5e0!3m2!1sfr!2sfr!4v1590676030979!5m2!1sfr!2sfr" width="600" height="850" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       </div>
 
     </div>
@@ -35,7 +74,9 @@
 </template>
 
 <script>
-import sal from 'sal.js'
+import fr from '../assets/datas/fr.json'
+import en from '../assets/datas/en.json'
+import sal from 'sal.js' 
 
 export default {
   layout: 'inside',
